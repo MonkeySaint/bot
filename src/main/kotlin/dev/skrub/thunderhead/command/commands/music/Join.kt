a0fce821cb0dedd833445396a6aacb98f09755d6
sourtaste000 <sourtaste000@gmail.com>
2021-07-29 10:35:47 -0700

    join and leave command
    Signed-off-by: sourtaste000 <sourtaste000@gmail.com>

---
+++ src/main/kotlin/dev/skrub/thunderhead/command/commands/music/Join.kt
@@ -0,0 +1,28 @@
+package dev.skrub.thunderhead.command.commands.music
+
+import dev.skrub.thunderhead.command.Command
+import dev.skrub.thunderhead.util.InfixUtil.sendMessageQueue
+import dev.skrub.thunderhead.util.MessageUtil
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+
+
+class Join :
+    Command("join", "Joins VC", listOf(), 3, listOf("connect")) {
+    override fun execute(args: List<String>, event: GuildMessageReceivedEvent) {
+        if (event.guild.audioManager.isConnected) {
+            event.message.channel.sendMessageQueue(MessageUtil.tell("I am already in a voice channel!"))
+            return
+        }
+
+        val joinedVC = event.message.member?.voiceState?.channel ?: kotlin.run {
+            event.message.channel.sendMessageQueue(
+                "Please join a voice channel first!"
+            ); return
+        }
+        try {
+            event.guild.audioManager.openAudioConnection(joinedVC)
+        } catch (e: Exception) {
+            event.message.channel.sendMessageQueue(MessageUtil.error("An internal error occurred! Please try again later."))
+        }
+    }
+}
---
+++ src/main/kotlin/dev/skrub/thunderhead/command/commands/music/Leave.kt
@@ -0,0 +1,12 @@
+package dev.skrub.thunderhead.command.commands.music
+
+import dev.skrub.thunderhead.command.Command
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+
+
+class Leave :
+    Command("leave", "Joins VC", listOf(), 3, listOf("disconnect")) {
+    override fun execute(args: List<String>, event: GuildMessageReceivedEvent) {
+        event.guild.audioManager.closeAudioConnection()
+    }
+}
