b629805e40ccc6d963342bb8c59316347b8f09b3
Humboldt123 <38266782+humboldt123@users.noreply.github.com>
2021-07-30 13:32:19 -0400

    updated emoji list

--- src/main/kotlin/dev/skrub/thunderhead/info/EmojiInfo.kt
+++ src/main/kotlin/dev/skrub/thunderhead/info/EmojiInfo.kt
@@ -1,8 +1,19 @@
 package dev.skrub.thunderhead.info
 
 object EmojiInfo {
-    val thunderhead = "<:thunderhead:808483944823652382>"
-    val currency = "<:vibes:779392920930222150>"
+    val thunderhead = "<:thunderhead:870715821134065714>"
+    val cirrus = "<:cirrus:870718358608314418>"
+    val currency = ":coin:"
     val check = "✅"
     val warn = "⚠"
+    val online = "<:online:870714303236440154>"
+    val idle = "<:idle:870714302758285313>"
+    val dnd = "<:dnd:870714303160909844>"
+    val offline = "<:offline:870714303072858173>"
+    val streaming = "<:streaming:870714304217886740>"
+    val desktop = "<:desktop:870714303081218098>"
+    val mobile = "<:mobile:870714303165128724>"
+    val web = "<:web:870714303106408478>"
+    val ping = "<:ping:870714302754074655>"
+    val bot = "<:bot:870714303077023784>"
 }
